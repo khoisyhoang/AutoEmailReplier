6a750835797d9b016eee15dde1c9eaf39bdf67ef
jaedon farr <jaedonfarr@gmail.com>
2025-03-01 19:01:34 -0500

    firebase initial stuff

---
+++ src/utils/server.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMrD5SKeL8PJuG-kQz49xHRVd4-xGFhB4",
+  authDomain: "repliesai-22011.firebaseapp.com",
+  projectId: "repliesai-22011",
+  storageBucket: "repliesai-22011.firebasestorage.app",
+  messagingSenderId: "979299555726",
+  appId: "1:979299555726:web:e2883a4b599ebb3f3f6cdf",
+};
+
+// Initialize Firebase
+export const firebase = initializeApp(firebaseConfig);
